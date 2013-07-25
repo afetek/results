374992c400450426e6458771a0b4da09b4d02752
George Rojas <George@TheRojas.com>
2013-07-24 22:08:24 -0500

    new results

---
+++ startup_mode_r3_test.py
@@ -0,0 +1,18 @@
+# Get Script Variables: timestamp: 2616589
+# generated script variable --> self.TEST_RUN = "r3": timestamp: 2616604
+# generated script variable --> self.fan1_fault = "False": timestamp: 2616624
+# generated script variable --> self.fan2_fault = "True": timestamp: 2616644
+# generated script variable --> self.description = "Fan1 ok Fan2 faulted at startup mode": timestamp: 2616664
+# Test Setup --> r3 Debug Level: 3: timestamp: 2616670
+# Start Test --> : timestamp: 2616928
+
+def test1_test_test():
+    """
+    script1 run r3 test 1: Exception in RunScript
+    """
+
+    test_passed = False
+    assert test_passed, "Failed test1_test"
+
+
+# Test Done --> r3: timestamp: 2616929
